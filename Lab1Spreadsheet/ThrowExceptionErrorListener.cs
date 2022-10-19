0fe78efc53d42ec604d8d20f21cc1c1b1dfb70ee
Yurii Pidlisnyi <yuriipidlisnyi2021@gmail.com>
2022-10-19 14:25:12 +0300

    Added error msg when entering invalid cell expression

--- Lab1Spreadsheet/ThrowExceptionErrorListener.cs
+++ Lab1Spreadsheet/ThrowExceptionErrorListener.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Lab1Spreadsheet
 {
@@ -16,6 +17,8 @@ namespace Lab1Spreadsheet
 
         public void SyntaxError(IRecognizer recognizer, int offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
         {
+            // Show that cell expression is invalid
+            MessageBox.Show("Не вірно введений вираз  для клітинки! Програма закривається", "Помилка!", MessageBoxButtons.OK);
             throw new ArgumentException("Invalid expression: {0}", msg, e);
         }
     }
